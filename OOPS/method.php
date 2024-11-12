cba34dcd4528cf53dde2033ce6baaa3214db26b6
Pranit Raut <89187326+PranitRaut@users.noreply.github.com>
2024-11-12 15:37:40 +0530

    Add files via upload

---
+++ OOPS/method.php
@@ -0,0 +1,68 @@
+<?php 
+
+
+// methods 
+
+// basic
+
+function info(){
+
+    echo("welcome to codes with pankaj");
+
+
+}
+
+// calling function
+
+info();
+info();
+
+// function with arg....
+function result($price,$gst){
+
+    $gst_amount = ($price*$gst)/100;
+    $final_price = $gst_amount+$price;
+
+    echo("<h1> GST :".$gst."</h1>");
+    echo("<h1> Price : ".$price."</h1>");
+    echo("<h1> GST Amount ".$gst_amount."</h1>");
+    echo("<h1> Final Price : ".$final_price."</h1>");
+
+}
+
+
+result(1200,18);
+
+
+# return type 
+
+function tax(){
+    return 1200;
+}
+
+
+
+// return type with arg...
+
+function bill($price,$gst){
+
+    $gst_amount = ($price*$gst)/100;
+    $final_price = $gst_amount+$price;
+
+    return $final_price;
+
+}
+
+
+?>
+
+
+<h1>
+
+    <?php echo("Tax : ".tax()); ?>
+</h1>
+
+<h1>
+
+    <?php echo("MRP : ".bill(4500,9)); ?>
+</h1>
